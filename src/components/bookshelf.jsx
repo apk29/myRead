7952ebe8526094d9478256112b92b810e3dcd9b7
Perry K <apk29@hotmail.com>
2018-10-22 11:54:07 -0700

    bookshelf

---
+++ src/components/bookshelf.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Bookshelf = () => {
+  return (
+    <main className="container">
+      <div className="list-books">
+        <h1 className="list-books-title">Your Digital Bookshelf</h1>
+        <div className="list-books-container">
+          
+        </div>
+        <div className="open-search">
+          
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Bookshelf;
