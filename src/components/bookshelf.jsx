ef144e94ccd7132ea3c3a4eb2ee3541b1b86647d
Perry K <apk29@hotmail.com>
2018-10-23 20:08:09 -0700

    recreated shelf

--- src/components/bookshelf.jsx
+++ src/components/bookshelf.jsx
@@ -7,8 +7,9 @@ const Bookshelf = ({ moveShelf, books}) => {
       <div className="list-books">
         <h1 className="list-books-title">MyReads</h1>
         <div className="list-books-container">
-        <OnShelf shelf={books} moveBook={moveShelf} />
-       
+        <OnShelf shelf={books.filter(b => b.shelf === 'currentlyReading' )} moveBook={moveShelf} />
+        <OnShelf shelf={books.filter(b => b.shelf === 'wantToRead' )} moveBook={moveShelf} />
+        <OnShelf shelf={books.filter(b => b.shelf === 'read' )} moveBook={moveShelf} />
         </div>
         <div className="open-search">
           
